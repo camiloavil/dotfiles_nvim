6c8806eebb457075d0ba6180ed4859333aef1988
Camilo Avila <camilo.avil@gmail.com>
2024-12-27 09:49:13 -0500

    set plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,36 +1,55 @@
 return {
   {
-    "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = require("configs.mason")
-    -- opts = {
-    --   ensure_installed = {
-    --     "black",
-    --     "mypy",
-    --     "pyright",
-    --     -- "ruff-lsp",
-    --     -- "debugpy",
-    --   },
-    -- },
-  },
-  {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
   {
-  	"nvim-treesitter/nvim-treesitter",
-    opts = require("configs.treesitter")
-  	-- opts = {
-  	-- 	ensure_installed = {
-  	-- 		"vim", "lua", "vimdoc",
-  	--     "html", "css", "python"
-  	-- 	},
-  	-- },
+    "williamboman/mason.nvim",
+    -- require("mason").setup {
+    --   ui = {
+    --     icons = {
+    --       package_installed = "✓",
+    --       package_pending = "➜",
+    --       package_uninstalled = "✗",
+    --     },
+    --   },
+    -- },
+    opts = require "configs.mason",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = require "configs.treesitter",
+  },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      -- "mfussenegger/nvim-dap",
+      -- "mfussenegger/nvim-dap-python", --optional
+      { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    lazy = false,
+    opts = {
+      name = { "venv", ".venv", "env", ".env" },
+      auto_refresh = true,
+    },
+    branch = "regexp", -- This is the regexp branch, use this for the new version
+    config = function()
+      require("venv-selector").setup()
+    end,
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { "<leader>vs", "<cmd>VenvSelect<cr>" },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+      -- { ",v", "<cmd>VenvSelect<cr>" },
+    },
   },
 }
