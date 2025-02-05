cefeb9448a224398eb6d49305aae28c72e56bd7d
Camilo Avila <camilo.avil@gmail.com>
2025-02-05 17:35:55 -0500

    feat: add Tmux navigation key mappings and improve existing mappings- Added new key mappings for navigating Tmux panes:  - <C-h> to navigate left  - <C-j> to navigate down  - <C-k> to navigate up  - <C-l> to navigate right- Updated existing mappings:  - Added description for exiting insert mode using 'jk'  - Added universal save buffer command for normal, insert, and visual modes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,12 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jk", "<ESC>", { desc = "CMD exit insert mode" })
+map({ "n", "i", "v" }, "<C-x>", "<cmd> w <cr>", { desc = "CMD save buffer" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Navigate left in Tmux" })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Navigate down in Tmux" })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Navigate up in Tmux" })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Navigate right in Tmux" })
