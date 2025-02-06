7754a8315202367ea845df947ff2a48740c7796d
Camilo Avila <camilo.avil@gmail.com>
2025-02-05 22:10:10 -0500

    Add Obsidian Plugins

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -7,6 +7,16 @@ local options = {
     "html",
     "css",
     "python",
+    "markdown",
+    "json",
+    "yaml",
+    "typescript",
+    "javascript",
+    "markdown_inline",
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
   },
 }
 
